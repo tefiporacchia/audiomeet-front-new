cae4fdbd09c0a0ff9b1adf2e8d0d1a4a484032a1
Manuela Masjoan <manuelamasjoan@MacBook-Air-de-Manuela.local>
2021-04-23 22:53:54 -0300

    corrections

--- src/components/data/Pictures.js
+++ src/components/data/Pictures.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import ReactDOM from "react-dom";
 import ImageUploading from "react-images-uploading";
 import '../../style/data/Pictures.scss';
@@ -24,17 +24,22 @@ const Pictures = () => {
 
 
     const [userImages, setUserImages] = React.useState([]);
+    const [loading, setLoading] = useState(false);
     const curUser = auth.currentUser;
     const storageRef = storage.ref();
 
     const submitData = event => {
+
         //add images to storage
         uploadToStorage()
+        setTimeout(addToArray,10000)
+        setTimeout(addToDatabase,20000)
         //Add images url to database
-        addToDatabase()
+
     }
 
     function uploadToStorage(){
+        setLoading(true)
 
         for (let i = 0; i < images.length; i++) {
             const message = images[i].data_url
@@ -42,23 +47,31 @@ const Pictures = () => {
                 console.log('Uploaded a data_url string!');
             });
         }
+
+        setLoading(true)
     }
 
-    function addToDatabase(){
-
-        //const userImages=[]
-        // Find all the images for user and add then to array.
+    function addToArray(){
         storageRef.child(curUser.email).listAll().then(function(res) {
             res.items.forEach(function(itemRef) {
                 itemRef.getDownloadURL().then(function(url){
-                    console.log("added "+ url)
-                    userImages.push(url)
+                    //setTimeout(function(){
+                        userImages.push(url)
+                        console.log("added "+ url)
+                    //},10000)
+
                 })
             });
         }).catch(function(error) {
             console.log('Error!');
         });
 
+    }
+
+    function addToDatabase(){
+
+        //const userImages=[]
+        // Find all the images for user and add then to array.
         console.log("userImages="+ userImages)
 
         //copy array to database
@@ -72,7 +85,20 @@ const Pictures = () => {
             .catch((error) => {
                 console.error("Error writing document: ", error);
             });
+
+        setLoading(false)
+
     }
+
+    /**
+    function load()
+    {
+        if(loading===true){
+            document.getElementsByClassName('loader').visibility = 'visible';
+        }else{
+            document.getElementsByClassName('loader').visibility = 'hidden';
+        }
+    }**/
 
     return (
         <div className="Pictures">
