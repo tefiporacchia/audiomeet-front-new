85a735754fb6f298121bd942599b1063125c0266
tefiporacchia <stefania.poracchia@ing.austral.edu.ar>
2021-04-27 19:14:09 -0300

    fixed stuff

--- src/components/home/TarjetasTinder.js
+++ src/components/home/TarjetasTinder.js
@@ -7,17 +7,20 @@ const TarjetasTinder = () => {
 
     const [persona,setPersona] = useState([]);
 
-    const [wantsFriendship, setWantsFriendship] = useState(false);
-    const [noDrinksPref,setNoDrinksPref] = useState(false);
-    const [yesDrinksPref,setYesDrinksPref] = useState(false);
-    const [noSmokesPref,setNoSmokesPref] = useState(false);
-    const [yesSmokesPref,setYesSmokesPref] = useState(false);
-    const [notSerious, setNotSerious] = useState(false);
-    const [yesSerious, setYesSerious] = useState(false);
-    const [olderThan,setOlderThan] = useState(0);
-    const [youngerThan,setYoungerThan] = useState(100);
-    const [cumpleCondicioness, setCumpleCondiciones]= useState(false);
+    const [wantsFriendship, setWantsFriendship] = useState(null);
+    const [noDrinksPref,setNoDrinksPref] = useState(null);
+    const [yesDrinksPref,setYesDrinksPref] = useState(null);
+    const [noSmokesPref,setNoSmokesPref] = useState(null);
+    const [yesSmokesPref,setYesSmokesPref] = useState(null);
+    const [notSerious, setNotSerious] = useState(null);
+    const [yesSerious, setYesSerious] = useState(null);
+    const [olderThan,setOlderThan] = useState(null);
+    const [youngerThan,setYoungerThan] = useState(null);
+    const [cumpleCondicioness, setCumpleCondiciones]= useState(null);
+    const [log, setLog] = useState(false);
 
+    const [usersPreferences, setUsersPreferences]= useState([]);
+    const [usersData, setUsersData]= useState([]);
 
     const database = firebaseApp.firestore();
     const curUser = auth.currentUser;
@@ -30,8 +33,6 @@ const TarjetasTinder = () => {
 
     useEffect(() => {
 
-        console.log(get_age('1982-09-20'));
-
         const docRef = database.collection("userPreferences").doc(curUser.email);
         docRef.get().then((doc) => {
             if (doc.exists) {
@@ -41,7 +42,7 @@ const TarjetasTinder = () => {
                 setNoDrinksPref(doc.data().noDrinks);
                 setNoSmokesPref(doc.data().noSmokes);
                 setYesSmokesPref(doc.data().yesSmokes);
-                setYesSerious(doc.data().yesSerious);
+                setYesSerious(doc.data().serious);
                 setNotSerious(doc.data().notSerious);
                 setOlderThan(doc.data().olderThan);
                 setYoungerThan(doc.data().youngerThn);
@@ -54,33 +55,19 @@ const TarjetasTinder = () => {
             console.log("Error getting document:", error);
         });
 
-        const userDataCollection = database.collection("userData")
+        const desuscribirse = database.collection('userData').onSnapshot(snapshot => (
+            setUsersData(snapshot.docs.map( doc => {return({id:doc.id, ...doc.data()})}))
+            //el documento "doc id" en userdata comparte las preferencias del usuario
+        ));
 
-        function cumpleCondiciones(docId){
-            const documento = userDataCollection.doc(docId);
-            documento.get().then((doc) => {
-                if (doc.exists) {
-                    if((doc.data().drinks==yesDrinksPref||doc.data().drinks!=noDrinksPref)
-                        &&(doc.data().smokes==yesSmokesPref||doc.data().smokes!=noSmokesPref)
-                        &&(get_age(doc.data().bday)>=olderThan)&&(get_age(doc.data().bday)<=youngerThan)){
-                        setCumpleCondiciones(true)
-                        console.log(get_age(doc.data().bday) + "holaa")
-                    }
+        const desuscribirse2 = database.collection('userPreferences').onSnapshot(snapshot => (
+            setUsersPreferences(snapshot.docs.map( doc => {return({id:doc.id, ...doc.data()})}))
+            //el documento "doc id" en userdata comparte las preferencias del usuario
+        ));
 
-                } else {
-                    // doc.data() will be undefined in this case
-                    console.log("No such document!");
-                }
-            }).catch((error) => {
-                console.log("Error getting document:", error);
-            });
-
-            return cumpleCondicioness;
-        }
-
-
+        /*console.log()
         const desuscribirse = database.collection('userImages').onSnapshot(snapshot => (
-            setPersona(snapshot.docs.map( doc => cumpleCondiciones(doc.id) && doc.data()))
+            setPersona(snapshot.docs.map( doc => !cumpleCondiciones(doc.id) && doc.data()).filter(elem => elem))
             //el documento "doc id" en userdata comparte las preferencias del usuario
         ));
 
@@ -91,9 +78,74 @@ const TarjetasTinder = () => {
 
         return () => {
             desuscribirse();
-        }
+        }*/
 
     }, [])
+
+    useEffect(() => {
+
+        if(wantsFriendship !=null && noDrinksPref!=null && yesDrinksPref!=null && noSmokesPref!=null && yesSmokesPref!=null
+            && notSerious!=null && yesSerious!=null && olderThan !=null && youngerThan!=null){
+
+            setLog(true);
+        }
+
+    }, [wantsFriendship, noDrinksPref, yesDrinksPref, noSmokesPref, yesSmokesPref, notSerious, yesSerious, olderThan,youngerThan])
+
+    useEffect(()=> {
+        if(log && usersData && usersPreferences){
+            const desuscribirse = database.collection('userImages').onSnapshot(snapshot => (
+                setPersona(snapshot.docs.map( doc => cumpleCondiciones(doc.id) && doc.data()).filter(elem => elem))
+                //el documento "doc id" en userdata comparte las preferencias del usuario
+            ));
+
+            console.log(persona.length); //hay 5
+            console.log(persona.filter( (ob) => ob.nombre.includes("Tefi"))) //para filtrar json
+
+            // const filtro1 = desuscribirse().filter( (auto) => auto.title.includes("Jeep"))
+
+            return () => {
+                desuscribirse();
+            }
+            console.log(cumpleCondiciones('manumasjoan@gmail.com'))
+        }
+    },[log, usersData, usersPreferences])
+
+    useEffect(()=>{
+
+    }, [persona])
+
+
+    function cumpleCondiciones(docId){
+        let cumple = false;
+
+        usersData.map(data => {if (data.id == docId) usersPreferences.map(preferences => {
+            if (preferences.id == docId) {
+                if((data.drinks==yesDrinksPref||data.drinks!=noDrinksPref)
+                    &&(data.smokes==yesSmokesPref||data.smokes!=noSmokesPref)
+                    &&(get_age(data.bday)>=olderThan)&&(get_age(data.bday)<=youngerThan) && docId!= curUser.email
+                    && (preferences.friendship==wantsFriendship ||preferences.serious==yesSerious || preferences.notSerious == notSerious)){
+                    cumple = true
+                    return 1
+                }else{
+                    console.log(data.drinks)
+                    console.log(yesDrinksPref)
+                    console.log(data.drinks) //manu no toma
+                    console.log(noDrinksPref) // quiero que tomen?
+                    console.log(data.smokes)
+                    console.log(yesSmokesPref)
+                    console.log(data.smokes) //manu no fuma
+                    console.log(noSmokesPref) // prefiero que fumen?
+                    console.log(get_age(data.bday)>=olderThan)
+                    console.log(get_age(data.bday)<=youngerThan)
+
+                    cumple=false
+                    return 1
+                }
+            }
+                return 1} )})
+        return cumple
+    }
 
     return (
         <div className="tarjetasTinder">
@@ -106,10 +158,10 @@ const TarjetasTinder = () => {
                         >
                         <div
                             className="tarjeta"
-                            style={{backgroundImage:`url(${persona.url})`}}
+                            style={{backgroundImage:`url(${persona.userImages ? persona.userImages[0] : undefined})`}}
                         >
-                            <h2>{persona.nombre}</h2>
-                            <h4>{persona.descripcion}</h4>
+                            <h2>{persona.username}</h2>
+                            <h4>{persona.description}</h4>
                         </div>
 
                         </TarjetaPersona>
