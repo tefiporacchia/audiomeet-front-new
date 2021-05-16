cf7f795ef0623d726cb25f2dbf1140498ac5cc0f
tefiporacchia <stefania.poracchia@ing.austral.edu.ar>
2021-05-16 19:00:49 -0300

    just to save

--- src/components/home/BotonesSwipe.js
+++ src/components/home/BotonesSwipe.js
@@ -8,99 +8,21 @@ import FlashOnIcon from '@material-ui/icons/FlashOn';
 import { IconButton } from '@material-ui/core';
 import firebaseApp, {auth} from "../../firebase";
 
-const BotonesSwipe = () => {
+const BotonesSwipe = ({undo, reject,like}) => {
 
-    const database = firebaseApp.firestore();
-    const curUser = auth.currentUser;
-
-
-    const [usersLiked, setUsersLiked]= useState([]);
-    const [myUsersLiked, setMyUsersLiked]= useState([]);
-    const [lastRejected,setLastRejected]= useState(null);
-
-
-    const desuscribirse3 = database.collection('usersLiked').onSnapshot(snapshot => (
-        setUsersLiked(snapshot.docs.map( doc => {return({id:doc.id, ...doc.data()})}))
-
-        //el documento "doc id" en userdata comparte las preferencias del usuario
-    ));
-
-    useEffect(()=>{
-        usersLiked.map(user => {
-            if(curUser===user.id){
-                setMyUsersLiked(user.myUsersLiked)
-            }
-        })
-
-    }, [usersLiked])
-
-
-
-    function reject(rejectedPerson){
-        if(rejectedPerson!==null){
-            setLastRejected(rejectedPerson);
-            //persona.shift();
-            //persona.pop(); si esta atras
-        }
-
-    }
-
-    function undo(){
-        if(lastRejected!=null){
-            //persona.push(lastRejected) si va de atras para adelante
-            //persona.unshift(lastRejected)
-        }
-    }
-
-
-    function like(likedPerson){
-
-        if(likedPerson!==null){
-            setLastRejected(null);
-            //console.log("MI PERSONA", persona)
-            myUsersLiked.push(likedPerson.id)
-            const usersILiked = myUsersLiked.map((obj)=> {return Object.assign({}, obj)});
-
-            database.collection("usersLiked").doc(curUser.email).set({
-                myUsersLiked: usersILiked
-
-            })
-                .then(() => {
-                    console.log("Document successfully written!");
-                })
-                .catch((error) => {
-                    console.error("Error writing document: ", error);
-                });
-
-            //persona.shift();
-            //persona.pop(); si esta atras
-        }
-
-
-
-    }
     return (
         <div className="botonesSwipe">
-            <IconButton className={"botonesSwipe__replay"} onClick={undo()}>
+            <IconButton className={"botonesSwipe__replay"} onClick={undo}>
                 <ReplayIcon style={{ fontSize: 40 }}/>
             </IconButton>
 
-            <IconButton className={"botonesSwipe__close"} >
+            <IconButton className={"botonesSwipe__close"} onClick={reject}>
                 <CloseIcon style={{ fontSize: 40 }}/>
             </IconButton>
 
-            {/*<IconButton className="botonesSwipe__star">
-                <StarIcon style={{ fontSize: 40 }}/>
-            </IconButton>*/}
-
-            <IconButton className={"botonesSwipe__fav"} >
+            <IconButton className={"botonesSwipe__fav"} onClick={like}>
                 <FavoriteIcon style={{ fontSize: 40 }}/>
             </IconButton>
-
-            {/*<IconButton className="botonesSwipe__flash">
-                <FlashOnIcon style={{ fontSize: 40 }}/>
-            </IconButton>*/}
-
 
         </div>
     )
--- src/components/home/TarjetasTinder.js
+++ src/components/home/TarjetasTinder.js
@@ -7,6 +7,8 @@ import {IconButton} from "@material-ui/core";
 import ReplayIcon from "@material-ui/icons/Replay";
 import CloseIcon from "@material-ui/icons/Close";
 import FavoriteIcon from "@material-ui/icons/Favorite";
+import Notification from "../extras/Notification";
+import BotonesSwipe from "./BotonesSwipe";
 const TarjetasTinder = () => {
 
     const database = firebaseApp.firestore();
@@ -41,17 +43,12 @@ const TarjetasTinder = () => {
     const [likes, setLikes]= useState([]);  //likes de liked person
     const [likedPerson, setLikedPerson]= useState(null);
 
+    //datos de matches
+    const [matchNotificationShowing, setMatchToShow] = useState(false);
 
-
-
-    function get_age(time){
-        const MILLISECONDS_IN_A_YEAR = 1000*60*60*24*365;
-        const date_array = time.split('-')
-        const years_elapsed = (new Date() - new Date(date_array[0],date_array[1],date_array[2]))/(MILLISECONDS_IN_A_YEAR);
-        return Math.trunc(years_elapsed); }
+    //------------------------------------------------------------------------------------------------------------
 
     useEffect(() => {
-
         const docRef = database.collection("userPreferences").doc(curUser.email);
         docRef.get().then((doc) => {
             if (doc.exists) {
@@ -67,75 +64,57 @@ const TarjetasTinder = () => {
                 setYoungerThan(doc.data().youngerThn);
                 setWantsMale(doc.data().wantsMale);
                 setWantsFemale(doc.data().wantsFemale);
-
-
             } else {
-                // doc.data() will be undefined in this case
                 console.log("No such document!");
             }
         }).catch((error) => {
             console.log("Error getting document:", error);
         });
-
-        //cargo todos los datos de los usuarios
-        const desuscribirse = database.collection('userData').onSnapshot(snapshot => (
-            setUsersData(snapshot.docs.map( doc => {return({id:doc.id, ...doc.data()})}))
-        ));
-
-        //cargo todas las preferencias de los usuarios
-        const desuscribirse2 = database.collection('userPreferences').onSnapshot(snapshot => (
-            setUsersPreferences(snapshot.docs.map( doc => {return({id:doc.id, ...doc.data()})}))
-        ));
-
-
-
-
     }, [])
 
+    //------------------------------------------------------------------------------------------------------------
+
     useEffect(() => {
-
-        if(wantsFriendship !=null && noDrinksPref!=null && yesDrinksPref!=null && noSmokesPref!=null && yesSmokesPref!=null
-            && notSerious!=null && yesSerious!=null && olderThan !=null && youngerThan!=null && wantsMale!=null && wantsFemale!=null){
-
+        if(wantsFriendship !=null && noDrinksPref!=null && yesDrinksPref!=null
+            && noSmokesPref!=null && yesSmokesPref!=null && notSerious!=null
+            && yesSerious!=null && olderThan !=null && youngerThan!=null
+            && wantsMale!=null && wantsFemale!=null){
             setLog(true);
         }
 
     }, [wantsFriendship, noDrinksPref, yesDrinksPref, noSmokesPref, yesSmokesPref, notSerious, yesSerious, olderThan,youngerThan,wantsMale,wantsFemale])
 
+    //------------------------------------------------------------------------------------------------------------
+
     useEffect(()=> {
         if(log && usersData && usersPreferences){
             const desuscribirse = database.collection('userImages').onSnapshot(snapshot => (
                 setPersona(snapshot.docs.map( doc => cumpleCondiciones(doc.id) && devolverObjetoAppendeado(doc.data(),doc.id)).filter(elem => elem))
-                //el documento "doc id" en userdata comparte las preferencias del usuario
+
             ));
-            console.log(persona);
-
-            console.log(persona.length); //hay 5
-            console.log(persona.filter( (ob) => ob.nombre.includes("Tefi"))) //para filtrar json
-
-            // const filtro1 = desuscribirse().filter( (auto) => auto.title.includes("Jeep"))
-
+            console.log(persona.filter( (ob) => ob.nombre.includes("Tefi")))
             return () => {
                 desuscribirse();
             }
-
         }
     },[log, usersData, usersPreferences])
 
 
+    //---------Funciones Auxiliares-------------------------------------------------------------------------------
 
-    function devolverObjetoAppendeado(objeto, id){
-        objeto.id = id;
-        return objeto;
-    }
+            function get_age(time){
+                const MILLISECONDS_IN_A_YEAR = 1000*60*60*24*365;
+                const date_array = time.split('-')
+                const years_elapsed = (new Date() - new Date(date_array[0],date_array[1],date_array[2]))/(MILLISECONDS_IN_A_YEAR);
+                return Math.trunc(years_elapsed);
+            }
 
+            function devolverObjetoAppendeado(objeto, id){
+                objeto.id = id;
+                return objeto;
+            }
 
-    function devolverObjetoAppendeado(objeto, id){
-        objeto.id = id;
-        return objeto;
-    }
-
-//BUSCO USUARIOS QUE CUMPLEN CON PREFERENCIAS
+    //-------Busco usuarios que cumplen con preferencias----------------------------------------------------------
 
     function cumpleCondiciones(docId){
         let cumple = false;
@@ -159,13 +138,9 @@ const TarjetasTinder = () => {
     }
 
 
+    //-------FUNCION DE BOTONES------------------------------------------------------------------------
 
-//FUNCION DE BOTONES
-
-//cargo todas las personas con sus likes
-    const desuscribirse3 = database.collection('usersLiked').onSnapshot(snapshot => (
-        setUsersLiked(snapshot.docs.map( doc => {return({id:doc.id, ...doc.data()})}))
-    ));
+    //-------Cargo todas las personas con sus likes----------------------------------------------------
 
     //busco mis likes
     useEffect(()=>{
@@ -177,35 +152,36 @@ const TarjetasTinder = () => {
 
     }, [usersLiked])
 
+    //--------FUNCIONES DE BOTONES---------------------------------------------------------------------
 
-    const reject = event =>{
-        if(persona[persona.length-1]!==null){
-            setLastRejected(persona[persona.length-1]);
-            //tengo que eliminar ultimo elemento
-            setPersona(persona.filter(({ id }) => id !== persona[persona.length-1].id))
-        }
-    }
+            const reject = event =>{
+                if(persona[persona.length-1]!==null){
+                    setLastRejected(persona[persona.length-1]);
+                    //eliminar ultimo elemento
+                    setPersona(persona.filter(({ id }) => id !== persona[persona.length-1].id))
+                }
+            }
 
-    const undo = event =>{
-        if(lastRejected!=null){
-            //tengo que agregar al final de arreglo
-            setPersona(oldArray => [...oldArray,lastRejected]);
-        }
-    }
+            const undo = event =>{
+                if(lastRejected!=null){
+                    //tengo que agregar al final de arreglo
+                    setPersona(oldArray => [...oldArray,lastRejected]);
+                }
+            }
 
+            const like = event => {
+                if(persona[persona.length-1]!==null){
+                    setLastRejected(null);
+                    console.log("MIS PERSONAS", persona.length)
+                    console.log("MI PERSONA", persona[persona.length-1].id)
+                    setMyUsersLiked(oldArray => [...oldArray, persona[persona.length-1].id]);
+                    //tengo que eliminar ultimo elemento
+                    setPersona(persona.filter(({ id }) => id !== persona[persona.length-1].id))
+                    setLikedPerson(persona[persona.length-1])
+                }
+            }
 
-    const like = event => {
-
-        if(persona[persona.length-1]!==null){
-            setLastRejected(null);
-            console.log("MIS PERSONAS", persona.length)
-            console.log("MI PERSONA", persona[persona.length-1].id)
-            setMyUsersLiked(oldArray => [...oldArray, persona[persona.length-1].id]);
-            //tengo que eliminar ultimo elemento
-            setPersona(persona.filter(({ id }) => id !== persona[persona.length-1].id))
-            setLikedPerson(persona[persona.length-1])
-        }
-    }
+    //-------------------------------------------------------------------------------------------------------
 
     //cargo usuarios likeados a la base de datos
     useEffect(()=>{
@@ -224,7 +200,6 @@ const TarjetasTinder = () => {
             });
 
     }, [myUsersLiked])
-
 
 
     useEffect(()=>{
@@ -261,11 +236,10 @@ const TarjetasTinder = () => {
 
 
 
-
-
     return (
-
         <div className="tarjetasTinder">
+
+            <Notification name={'stef'}/>
 
             <div className="tarjetasTinder__contenedor">
                 {persona.map(persona => (
@@ -281,35 +255,12 @@ const TarjetasTinder = () => {
                             <h2>{usersData.find(x => x.id === persona.id).username}</h2>
                             <h4>{usersData.find(x => x.id === persona.id).description}</h4>
                         </div>
-
-
-
                     </TarjetaPersona>
-
-                    //conseguir las caracteristicas que el usuario pide
-                    //recorrer userData y mostrarle al usuario solo los que tienen las características que el usuario pide.
-                    //mostrar solo la primer foto
-
-
-
                 ))}
-
             </div>
 
-            <div className="botonesSwipe">
-                <IconButton className={"botonesSwipe__replay"} onClick={undo}>
-                    <ReplayIcon style={{ fontSize: 40 }}/>
-                </IconButton>
+            <BotonesSwipe undo={undo} reject={reject} like={like}/>
 
-                <IconButton className={"botonesSwipe__close"} onClick={reject}>
-                    <CloseIcon style={{ fontSize: 40 }}/>
-                </IconButton>
-
-                <IconButton className={"botonesSwipe__fav"} onClick={like}>
-                    <FavoriteIcon style={{ fontSize: 40 }}/>
-                </IconButton>
-
-            </div>
         </div>
 
 
